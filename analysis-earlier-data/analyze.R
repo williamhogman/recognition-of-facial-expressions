19373ffd3859150bb65ae0eb7c5d05a76c19b0a9
William Hogman <me@whn.se>
2015-02-21 13:51:30 +0100

    Add tables with results from first study

--- analysis-earlier-data/analyze.R
+++ analysis-earlier-data/analyze.R
@@ -1,4 +1,6 @@
 
+proper.emotion.names <- c("Disgust", "Contempt", "Surprise", "Happiness", "Anger", "Fear", "Sadness")
+
 
 recode.response <- function(response){
   f <- as.factor(response)
@@ -6,18 +8,63 @@ recode.response <- function(response){
   f
 }
 
-preprocess.frame <- function(df) {
-  colnames(df) <- sapply(colnames(df), tolower)
-  df$response <- recode.response(df$response)
+preprocess.frame <- function(dt) {
+  colnames(dt) <- sapply(colnames(dt), tolower)
+  dt$response <- recode.response(dt$response)
   # Merge levels exactly!
-  df$response <- factor(dt$response, levels=levels(dt$emotion))
-  df
+  dt$response <- factor(dt$response, levels=levels(dt$emotion))
+
+  # Use English labels
+  levels(dt$response) <-  proper.emotion.names
+  levels(dt$emotion) <- proper.emotion.names
+  
+  dt
 }
 
 get.confusion.matrix <- function(df){
-  olibrary(caret)
+  library(caret)
   tab <- table(df$response, df$emotion)
   confusionMatrix(tab)
 }
 
+mtx.for.file <- function(fn) {
+  get.confusion.matrix(preprocess.frame(read.csv(fn)))
+}
 
+
+study.one.data <- function() {
+  library(ascii)
+  options(asciiType="org")
+  source("analyze.R")
+  mtx <- mtx.for.file("annasdata.csv") 
+  print(ascii(as.data.frame(mtx$byClass)))
+  print(ascii(mtx$table))
+  mtx
+}
+
+
+latex.study1.confusion.matrix <- function(cm) {
+  library(xtable)
+  caps = c("Confusion matrix of responses in study 1, rows represent participant responses and columns represent the stimulus.", "Confusion matrix of responses in study 1")
+  xt <- xtable(cm$table, caption=caps, label="study1confusion")
+  print.xtable(xt, caption.placement="top", booktabs=T, tabular.environment="tabularx", width="\\textwidth")
+}
+
+latex.study1.outcomes <- function(cm) {
+  library(xtable)
+  caps = "Summary table of confusion for each class"
+  xt <- xtable(cm$byClass, caption=caps, label="study1classtable", align="lXXXXXXXX")
+  print.xtable(xt,
+               caption.placement="top",
+               booktabs=T,
+               tabular.environment="tabularx",
+               width="\\textwidth",
+               sanitize.rownames.function=function(x){
+                 gsub("Class: ", "", x)
+               },
+               sanitize.colnames.function=function(x){
+                 paste("\\rotatebox{-60}{", x, "}")
+               },
+               rotate.colnames=T
+               )
+}
